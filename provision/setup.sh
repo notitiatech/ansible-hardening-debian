2f283a521b0b5f9c65b10fcfd8a65050ba34e08b
Sandro Simeth <sandro@MacBook-Pro-von-Sandro.local>
2022-02-04 09:48:42 -0600

    ok

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 apt-get update
-apt-get -y install python python-pexpect software-properties-common --no-install-recommends
+apt-get -y install python python-pexpect software-properties-common git --no-install-recommends
 apt-add-repository --yes --update ppa:ansible/ansible
 apt-get -y install ansible --no-install-recommends
+
+cd /etc/ansible/roles/ && git clone https://github.com/notitiatech/ansible-hardening-debian.git
+ansible-playbooks /etc/ansible/roles/ansible-hardening-debian/provision/playbook-hardening.yml

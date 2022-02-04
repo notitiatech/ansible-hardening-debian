2e4be3fc0727eb0bb96784d4ba8b9d67737908e2
Sandro Simeth <sandro@MacBook-Pro-von-Sandro.local>
2022-02-04 10:12:26 -0600

    ok

--- provision/setup.sh
+++ provision/setup.sh
@@ -5,4 +5,4 @@ apt-add-repository --yes --update ppa:ansible/ansible
 apt-get -y install ansible --no-install-recommends
 
 cd /etc/ansible/roles/ && git clone https://github.com/notitiatech/ansible-hardening-debian.git
-ansible-playbooks /etc/ansible/roles/ansible-hardening-debian/provision/playbook-hardening.yml
+ansible-playbook /etc/ansible/roles/ansible-hardening-debian/provision/playbook-hardening.yml
